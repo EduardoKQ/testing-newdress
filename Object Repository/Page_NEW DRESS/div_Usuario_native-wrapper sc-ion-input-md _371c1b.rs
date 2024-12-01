4567c96d40cc68041bae906743d37e846610c44a
ke.sotof <ke.sotof@duocuc.cl>
2024-12-01 16:27:37 -0300

    aa

---
+++ Object Repository/Page_NEW DRESS/div_Usuario_native-wrapper sc-ion-input-md _371c1b.rs
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Usuario_native-wrapper sc-ion-input-md _371c1b</name>
+   <tag></tag>
+   <elementGuidId>6f74083e-cdfc-4c26-a68a-0e03c5493ef4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.native-wrapper.sc-ion-input-md.sc-ion-input-md-s</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Usuario'])[2]/following::div[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>.native-wrapper >> nth=0</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>a1c847fb-40f6-41f6-990f-ce831dbe2769</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>native-wrapper sc-ion-input-md sc-ion-input-md-s</value>
+      <webElementGuid>8a5c888f-83d2-439d-a3f6-0f56cd63c10f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;plt-desktop md hydrated&quot;]/body[1]/app-root[1]/ion-app[@class=&quot;md ion-page hydrated&quot;]/ion-router-outlet[@class=&quot;hydrated&quot;]/app-login[@class=&quot;ion-page&quot;]/ion-content[@class=&quot;md content-ltr hydrated&quot;]/ion-list[@class=&quot;list ion-padding md list-md list-lines-none list-md-lines-none hydrated&quot;]/ion-input[@class=&quot;custom ng-untouched ng-pristine ng-valid ion-untouched ion-pristine ion-valid sc-ion-input-md-h sc-ion-input-md-s md input-fill-outline input-label-placement-floating hydrated&quot;]/label[@class=&quot;input-wrapper sc-ion-input-md&quot;]/div[@class=&quot;native-wrapper sc-ion-input-md sc-ion-input-md-s&quot;]</value>
+      <webElementGuid>e0497680-ccf1-4376-b52b-cb5d164dc634</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Usuario'])[2]/following::div[1]</value>
+      <webElementGuid>d912e311-fa54-49fd-ac64-9cef76b397ee</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ingresa a tu cuenta'])[1]/following::div[8]</value>
+      <webElementGuid>f343e72e-5632-465b-91d3-0f4172c5d8d7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Contraseña'])[2]/preceding::div[6]</value>
+      <webElementGuid>3406bf74-4a66-4a11-b7ae-61514313d05c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='¿Olvidaste la Contraseña?'])[1]/preceding::div[9]</value>
+      <webElementGuid>81c56b8b-fc82-43a3-9bb9-f05aa4ff8bb6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]</value>
+      <webElementGuid>d7e33ec4-4713-4a06-99ae-9e86e404110b</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
